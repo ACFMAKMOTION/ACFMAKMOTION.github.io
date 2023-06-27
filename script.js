623685d55fbc3316d9659d0dafc5c6dda709c889
Tai N. Marufi <86039298+alhpacreative@users.noreply.github.com>
2023-06-27 14:44:17 -0400

    first commet

---
+++ script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assessment</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Assessment Page</h1>
+    </header>
+
+    <main>
+        <p>This is where the assessment would go.</p>
+    </main>
+
+    <footer>
+        <p>Copyright &copy; 2023</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
